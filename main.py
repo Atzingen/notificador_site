b7a19c259731ecba728a6ff8fafb4cec265984ac
Atzingen <gustavo.von.atzingen@gmail.com>
2022-11-28 16:43:58 -0300

    base main.py and telegram access

---
+++ main.py
@@ -0,0 +1,11 @@
+
+import requests
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+site_url = 'http://www.finep.gov.br/chamadas-publicas/chamadapublica/702'
+
+
+
+
