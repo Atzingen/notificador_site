1080b7cf0bb4275aca382e1794c07c84edcf7da4
Gustavo Voltani von Atzingen <gustavo.von.atzingen@gmail.com>
2022-12-02 23:58:52 -0300

    table criations

--- db_manager.py
+++ db_manager.py
@@ -4,6 +4,22 @@ from sqlalchemy import Table, Column, Integer, String
 
 
 engine = db.create_engine('sqlite:///base.db')
-
 metadata_obj = MetaData()
+conn = engine.connect()
 
+user = Table(
+    "notification",
+    metadata_obj,
+    Column("user_telegram_id", Integer, nullable=False),
+    Column("site_name", String(100), nullable=False),
+    Column("site_url", String(200), nullable=False),
+    Column("site_hash", String(300), nullable=False),
+)
+
+def create_table():
+    metadata_obj.create_all(engine)
+
+def insert_notification(user_id: int, site_name: str, 
+                        site_url: str, site_hash: str):  
+    query = db.insert(user).values(user_telegram_id=user_id, site_name=site_name, site_url=site_url, site_hash=site_hash)
+    conn.execute(query)
