6081ed2c8eec9e5a363d4871e685fbf49bb4fa87
Atzingen <gustavo.von.atzingen@gmail.com>
2025-03-11 14:08:36 -0300

    update nas funcoes\

--- db_manager.py
+++ db_manager.py
@@ -1,6 +1,6 @@
 import sqlalchemy as db
 from sqlalchemy import MetaData
-from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy import Table, Column, Integer, String, select, and_
 
 engine = db.create_engine('sqlite:///base.db')
 metadata_obj = MetaData()
@@ -19,33 +19,34 @@ def create_table():
     
 def get_all_sites():
     conn = engine.connect()
-    query = db.select([user.columns.user_telegram_id, 
-                       user.columns.site_name,
-                       user.columns.site_url, 
-                       user.columns.site_hash])
+    query = select(user.columns.user_telegram_id, 
+                   user.columns.site_name,
+                   user.columns.site_url, 
+                   user.columns.site_hash)
     result = conn.execute(query).all()
     conn.close()
     return result
 
 def check_user_exists(user_id: int):
     conn = engine.connect()
-    query = db.select([user]).where(user.c.user_telegram_id == user_id)
-    result = conn.execute(query).all() != []
+    query = db.select(user).where(user.c.user_telegram_id == user_id)
+    # result = conn.execute(query).all() != []
+    result = conn.execute(query).first() is not None
     conn.close()
     return result
     
 def get_notifications_list(user_id: int):
     conn = engine.connect()
-    query = db.select([user.columns.site_name, user.columns.site_url]).\
-                       where(user.c.user_telegram_id == user_id)
+    query = select(user.columns.site_name, user.columns.site_url).\
+                   where(user.c.user_telegram_id == user_id)
     result = conn.execute(query).all()
     conn.close()
     return result
 
 def get_site_hash(user_id: int, site_name: str):
     conn = engine.connect()
-    query = db.select([user]).where(user.c.user_telegram_id == user_id).\
-                              where(user.c.site_name == site_name)
+    query = select(user).where(and_(user.c.user_telegram_id == user_id,
+                                    user.c.site_name == site_name))
     result = conn.execute(query).fetchone()
     conn.close()
     return result
@@ -56,6 +57,7 @@ def insert_notification(user_id: int, site_name: str,
     query = db.insert(user).values(user_telegram_id=user_id, site_name=site_name, 
                                    site_url=site_url, site_hash=site_hash)
     result = conn.execute(query)
+    conn.commit()
     conn.close()
     return result
     
@@ -63,6 +65,7 @@ def delete_notification(user_id: int, site_name: str):
     conn = engine.connect()
     query = db.delete(user).where(user.c.user_telegram_id == user_id).where(user.c.site_name == site_name)
     result = conn.execute(query).rowcount > 0
+    conn.commit()
     conn.close()
     return result
 
@@ -70,5 +73,6 @@ def delete_user(user_id: int):
     conn = engine.connect()
     query = db.delete(user).where(user.c.user_telegram_id == user_id)
     result = conn.execute(query).rowcount > 0
+    conn.commit()
     conn.close()
     return result
